0ee48f498e6a3cc99e8472257d44d8a8f165fa2e
artemov <artik3314@gmail.com>
2024-10-17 22:21:44 +0300

    .

--- SportStore.Angular/src/services/userslocal.service.ts
+++ SportStore.Angular/src/services/userslocal.service.ts
@@ -7,9 +7,9 @@ import User from '../models/user'
 export class UsersLocalService {
 
   getLocalUsers(): User[] {
-    var users = [{ "id": "1", "name": "user1" },
-                 { "id": "2", "name": "user2" },
-                 { "id": "3", "name": "user3" }]
+    var users = [{ "id": 1, "name": "user1", "login": "user1" },
+                 { "id": 2, "name": "user2", "login": "user2" },
+                 { "id": 3, "name": "user3", "login": "user3" }]
     return users;
 
   }
