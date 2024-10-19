e0e3722344b0ec5b2dbad9830fb8f59d66ab83a2
artemov <artik3314@gmail.com>
2024-10-20 00:58:55 +0300

    Авторизация и регистрация

--- SportStore.Angular/src/components/auth/auth.component.ts
+++ SportStore.Angular/src/components/auth/auth.component.ts
@@ -3,24 +3,25 @@ import { FormsModule } from '@angular/forms';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { AuthService } from '../auth.service';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [FormsModule, MatInputModule, MatFormField, MatLabel, MatIcon],
+  imports: [CommonModule, FormsModule, MatInputModule, MatFormField, MatLabel, MatIcon],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
 export class AuthComponent {
 
   model:any = {}
-
-  constructor(private authService: AuthService) {
+  constructor(public authService: AuthService) {
   }
 
   login(){
-    this.authService.login(this.model)
+    console.log("login")
+    this.authService.login(this.model).subscribe({next: r => console.log(r), error: e => console.log(e)})
   }
 
   sign(){
@@ -29,7 +30,7 @@ export class AuthComponent {
   }
 
   logout(){
-
+    this.authService.logout()
   }
   
 
