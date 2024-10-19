d79a1087703a6b9d75d45c4d85cd7051763b9224
artemov <artik3314@gmail.com>
2024-10-20 00:59:41 +0300

    fix

---
+++ SportStore.Angular/src/services/auth.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import User from '../models/user';
+import { map, ReplaySubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  baseUrl:String = "http://localhost:5295/"
+
+  private currentUserSource = new ReplaySubject<User>(1);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http:HttpClient) { }
+
+  login(model:any){
+    
+    return this.http.post<User>(this.baseUrl + "Users/Login", model).pipe(
+      map((response: User) => {
+        const user = response;
+        if(user){
+          localStorage.setItem("user",JSON.stringify(user))
+          this.currentUserSource.next(user);
+          console.log(user)
+        }
+        else{
+          console.log(response)
+        }
+      })
+    )
+  }
+
+  register(model:any){
+    return this.http.post(this.baseUrl +"Users/", model)
+  }
+
+  logout(){
+    localStorage.removeItem("user")
+    this.currentUserSource.next(null!);
+  }
+
+}
