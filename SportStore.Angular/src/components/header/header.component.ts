f0457eeb8fe51a85b81e866503aef97b6e9453f0
artemov <artik3314@gmail.com>
2024-10-20 00:58:55 +0300

    Авторизация и регистрация

--- SportStore.Angular/src/components/header/header.component.ts
+++ SportStore.Angular/src/components/header/header.component.ts
@@ -3,14 +3,19 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIconModule, MatToolbarModule, MatButtonModule, RouterLink],
+  imports: [CommonModule, MatIconModule, MatToolbarModule, MatButtonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  constructor(public authService:AuthService) {
+  }
 
 }
