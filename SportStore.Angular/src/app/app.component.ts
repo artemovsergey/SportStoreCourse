4b071ccd62de9ca49973ed1ec6fca4d7f3b009fa
artemov <artik3314@gmail.com>
2024-11-23 09:20:45 +0300

    Настройка глобального спиннера на запросы с анимацией

--- SportStore.Angular/src/app/app.component.ts
+++ SportStore.Angular/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "../components/header/header.component";
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NgxSpinnerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- SportStore.Angular/src/app/app.config.ts
+++ SportStore.Angular/src/app/app.config.ts
@@ -7,6 +7,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideToastr } from 'ngx-toastr';
 import { errorInterceptor } from '../_interceptors/error.interceptor';
 import { jwtInterceptor } from '../_interceptors/jwt.interceptor';
+import { loadingInterceptor } from '../_interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,7 +20,7 @@ export const appConfig: ApplicationConfig = {
       preventDuplicates: true,
     }), 
     provideHttpClient(
-      withInterceptors([errorInterceptor, jwtInterceptor]),
+      withInterceptors([errorInterceptor, jwtInterceptor, loadingInterceptor]),
     ),
     provideAnimationsAsync()
 
